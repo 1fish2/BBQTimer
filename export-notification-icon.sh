1c48bf8f7d1234b4c49d980d4ea1d42721c8f3e4
Jerry Morrison <jerry.morrison@gmail.fake>
2014-03-10 23:45:16 -0700

    Add notification-icon.svg, exporter script, and exported .png files.
    NOTE: Inkscape always displays the doc over a white canvas, so if the doc
    is white shapes on transparent, you can't see anything!
    
    WORKAROUND: I added a black background layer to view while editing. So hide
    it, save the .svg, run the export script, unhide the background layer, then
    save it again.

---
+++ export-notification-icon.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Converts the Inkscape notification icon file to the app res png files at multiple resolutions.
+#
+# NOTE: Save the doc with the "background WHILE EDITING" layer hidden, then export, then make
+# that layer visible again so the icon doesn't look (invisible) white on white.
+# Unfortunately, Inkscape can't set the doc background to transparent and display it over something
+# besides a white canvas.
+
+PROJECT="BBQ Timer"
+IMAGE="notification-icon"
+INPUT="source-assets/${IMAGE}.svg"
+OUTPUT="${IMAGE}.png"
+MAIN="${PROJECT}/src/main/"
+RES="${MAIN}res/"
+DRAWABLE="${RES}/drawable"
+
+inkscape --shell <<COMMANDS
+  --export-png "${DRAWABLE}-mdpi/${OUTPUT}"   -w  24 "${INPUT}"
+  --export-png "${DRAWABLE}-hdpi/${OUTPUT}"   -w  36 "${INPUT}"
+  --export-png "${DRAWABLE}-xhdpi/${OUTPUT}"  -w  48 "${INPUT}"
+  --export-png "${DRAWABLE}-xxhdpi/${OUTPUT}" -w  72 "${INPUT}"
+quit
+COMMANDS
