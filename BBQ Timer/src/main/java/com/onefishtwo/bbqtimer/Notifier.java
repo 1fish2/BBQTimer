b48bb7595c758ac04a3dc75410dbdc737f19faa4
Jerry Morrison <jerry.morrison@gmail.fake>
2014-03-15 21:10:20 -0700

    Builder-style Notifier setters for visible, audible, & tactile notifications

--- BBQ Timer/src/main/java/com/onefishtwo/bbqtimer/Notifier.java
+++ BBQ Timer/src/main/java/com/onefishtwo/bbqtimer/Notifier.java
@@ -19,6 +19,7 @@
 
 package com.onefishtwo.bbqtimer;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -31,37 +32,96 @@ import android.support.v4.app.TaskStackBuilder;
  */
 public class Notifier {
     private static final int NOTIFICATION_ID = 7;
+
     private final Context context;
+    private boolean showNotification = true;
+    private boolean playChime = false;
+    private boolean vibrateWithChime = true;
 
     public Notifier(Context context) {
         this.context = context;
     }
 
-    /** Opens this app's notification. */
+    /**
+     * Builder-style setter: Whether {@link #open(TimeCounter)} should display the notification in
+     * the notification area and drawer. Default = true. {@link #open(TimeCounter)} requires
+     * {@code showNotification || playChime}.
+     */
+    public Notifier setShowNotification(boolean showNotification) {
+        this.showNotification = showNotification;
+        return this;
+    }
+
+    /**
+     * Builder-style setter: Whether {@link #open(TimeCounter)} should play a notification chime,
+     * light flash, and optionally vibrate. Default = false. {@link #open(TimeCounter)} requires
+     * {@code showNotification || playChime}.
+     */
+    public Notifier setPlayChime(boolean playChime) {
+        this.playChime = playChime;
+        return this;
+    }
+
+    /**
+     * Builder-style setter: Whether to vibrate when playing a notification chime. Default = true.
+     */
+    public Notifier setVibrateWithChime(boolean vibrateWithChime) {
+        this.vibrateWithChime = vibrateWithChime;
+        return this;
+    }
+
+    /**
+     * Opens this app's notification. Use {@link #setShowNotification(boolean)},
+     * {@link #setPlayChime(boolean)}, and {@link #setVibrateWithChime(boolean)} to control the
+     * notification's content.
+     *
+     * @param timer -- the TimeCounter state to display.
+     */
     public void open(TimeCounter timer) {
-        // TODO: Play a custom sound for a periodic chime. MP3 Mono/Stereo 8-320Kbps CBR or VBR.
-        // TODO: Lights & optional vibration with the chime. (Requires the VIBRATE permission.)
+        if (!(showNotification || playChime)) {
+            throw new IllegalArgumentException("Notifier requires showNotification || playChime");
+        }
+
+        // TODO: Play a custom chime sound. OGG/MP3 Mono/Stereo 8-320Kbps CBR or VBR.
         // TODO: Large icon: mdpi 64x64 px, hdpi 96x96 px, xhdpi 128x128 px, xxhpdi 192x192 px.
         // TODO: Add button actions to pause/resume/reset the timer?
         // TODO: builder.setNumber(chimeCount)?
         // TODO: builder.setContentText()?
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setSmallIcon(R.drawable.notification_icon)
-                .setContentTitle(context.getString(R.string.app_name))
                 // Use PRIORITY_MAX when there's a time-critical chime?
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setOngoing(true)
-                .setWhen(System.currentTimeMillis() - timer.getElapsedTime())
-                .setUsesChronometer(true);
-        Intent activityIntent = new Intent(context, MainActivity.class);
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
 
-        // So navigating back from the Activity goes from the app to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context)
-                .addParentStack(MainActivity.class)
-                .addNextIntent(activityIntent);
-        PendingIntent activityPendingIntent =
-                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(activityPendingIntent);
+        if (showNotification) {
+            builder.setSmallIcon(R.drawable.notification_icon)
+                    .setContentTitle(context.getString(R.string.app_name))
+                    .setOngoing(true)
+                    .setWhen(System.currentTimeMillis() - timer.getElapsedTime())
+                    .setUsesChronometer(true);
+
+            // Make an Intent to launch the Activity from the notification.
+            Intent activityIntent = new Intent(context, MainActivity.class);
+
+            // So navigating back from the Activity goes from the app to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context)
+                    .addParentStack(MainActivity.class)
+                    .addNextIntent(activityIntent);
+            PendingIntent activityPendingIntent =
+                    stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(activityPendingIntent);
+        }
+
+        if (playChime) {
+            int defaults = Notification.DEFAULT_LIGHTS;
+
+            defaults |= Notification.DEFAULT_SOUND; // TODO: builder.setSound() instead.
+            // Uri chimeUri = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.tone);
+            // builder.setSound(chimeUri);
+
+            if (vibrateWithChime) {
+                defaults |= Notification.DEFAULT_VIBRATE;
+            }
+            builder.setDefaults(defaults);
+        }
 
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
