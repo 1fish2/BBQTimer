1e764b1194090f69b4737c975c00acb3e972e87a
Jerry Morrison <jerry.morrison@gmail.fake>
2014-06-29 00:57:19 -0700

    add an optional arg to uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # Uninstalls the APK from a connected device or emulator.
+#
+# Arguments:
+#   $1 -- optional flags like "-d" to force it to use an ADB USB Device.
 
-adb uninstall com.onefishtwo.bbqtimer
+adb "${1-}" uninstall com.onefishtwo.bbqtimer
