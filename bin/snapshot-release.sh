985ddf58d588130f14a1c62ee079cc2a9a486e8c
Jerry Morrison <1fish2@users.noreply.github.com>
2016-08-26 21:25:26 -0700

    delete release snapshots from the repo
    Save the snapshots just in GitHub releases and Play Store uploads.

--- bin/snapshot-release.sh
+++ bin/snapshot-release.sh
@@ -5,6 +5,6 @@
 #   $1 -- a release name (defaults to the current date)
 
 DIR="BBQ Timer/build/outputs"
-OUTPUT="releases/snapshot_${1:-`date "+%Y%m%d"`}.tar.bz2"
+OUTPUT="tmp/snapshot_${1:-`date "+%Y%m%d"`}.tar.bz2"
 tar cjvf "${OUTPUT}" "${DIR}/apk/BBQ Timer-release.apk" \
     "${DIR}/mapping/release/mapping.txt"
