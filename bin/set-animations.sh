5e6fdf51bf7a504aeff2601d66c2b83e55c6731c
Jerry Morrison <1fish2@users.noreply.github.com>
2022-08-19 16:51:03 -0700

    show pre/post state in bin/set-animations.sh

--- bin/set-animations.sh
+++ bin/set-animations.sh
@@ -6,8 +6,18 @@
 
 SCALE="${1:-0.0}"
 
+echo window_animation_scale=`adb -e shell settings get global window_animation_scale`
+echo transition_animation_scale=`adb -e shell settings get global transition_animation_scale`
+echo animator_duration_scale=`adb -e shell settings get global animator_duration_scale`
+
+echo
 echo Setting Android animation time scale to $SCALE
+echo
 
 adb -e shell settings put global window_animation_scale $SCALE
 adb -e shell settings put global transition_animation_scale $SCALE
 adb -e shell settings put global animator_duration_scale $SCALE
+
+echo window_animation_scale=`adb -e shell settings get global window_animation_scale`
+echo transition_animation_scale=`adb -e shell settings get global transition_animation_scale`
+echo animator_duration_scale=`adb -e shell settings get global animator_duration_scale`
