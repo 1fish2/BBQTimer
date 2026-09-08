ebb85a61acba92c30d481eda64c559ac0fce2cc1
Jerry Morrison <1fish2@users.noreply.github.com>
2026-09-08 00:42:37 -0700

    LocaleUtilsTest fixes
    * Use the newer `Locale.forLanguageTag()` factory method.
    * Initialize the default and FORMAT locales for a deterministic test.
    * Split into smaller test methods.
    * Add coverage for negative and freezing temperatures.

--- app/src/test/java/com/onefishtwo/bbqtimer/LocaleUtilsTest.java
+++ app/src/test/java/com/onefishtwo/bbqtimer/LocaleUtilsTest.java
@@ -15,12 +15,8 @@ import java.util.Locale;
 
 // In a unit test, Build.VERSION.SDK_INT == 0.
 public class LocaleUtilsTest {
-    /** @noinspection RedundantSuppression*/
-    @SuppressWarnings("deprecation") // for Gradle
-    private static final Locale BAHAMAS = new Locale("en", "BS");
-    /** @noinspection RedundantSuppression*/
-    @SuppressWarnings("deprecation")
-    private static final Locale SPAIN = new Locale("es", "ES");
+    private static final Locale BAHAMAS = Locale.forLanguageTag("es-BS");
+    private static final Locale SPAIN = Locale.forLanguageTag("es-ES");
 
     private Locale initialLocale, initialFormatLocale;
 
@@ -28,12 +24,19 @@ public class LocaleUtilsTest {
     public void setUp() {
         initialLocale = Locale.getDefault();
         initialFormatLocale = Locale.getDefault(Locale.Category.FORMAT);
+
+        setSystemLocales(Locale.US); // deterministic test
     }
 
     @After
     public void tearDown() {
         Locale.setDefault(initialLocale);
         Locale.setDefault(Locale.Category.FORMAT, initialFormatLocale);
+    }
+
+    private void setSystemLocales(Locale locale) {
+        Locale.setDefault(locale);
+        Locale.setDefault(Locale.Category.FORMAT, locale);
     }
 
     @Test
@@ -45,13 +48,16 @@ public class LocaleUtilsTest {
     }
 
     @Test
-    public void testUseFahrenheit() {
+    public void testUseFahrenheit_withExplicitLocal() {
         assertTrue(useFahrenheit(Locale.US));
         assertTrue(useFahrenheit(BAHAMAS));
 
         assertFalse(useFahrenheit(Locale.GERMANY));
         assertFalse(useFahrenheit(SPAIN));
+    }
 
+    @Test
+    public void testUseFahrenheit_withDefaultLocale() {
         assertTrue(useFahrenheit());
 
         Locale.setDefault(Locale.UK);
@@ -59,27 +65,45 @@ public class LocaleUtilsTest {
     }
 
     @Test
-    public void testFormatTemperatureFromFahrenheit() {
+    public void testFormatTemperatureFromFahrenheit_inFahrenheit() {
         assertEquals("32°F", formatTemperatureFromFahrenheit(32.0));
         assertEquals("212°F", formatTemperatureFromFahrenheit(212.0));
         assertEquals("145°F", formatTemperatureFromFahrenheit(145.0));
         assertEquals("165°F", formatTemperatureFromFahrenheit(165.0));
+    }
 
-        // Test rounding
+    @Test
+    public void testFormatTemperatureFromFahrenheit_inCelsius() {
+        setSystemLocales(Locale.GERMANY);
+
+        assertEquals("0°C", formatTemperatureFromFahrenheit(32.0));
+        assertEquals("100°C", formatTemperatureFromFahrenheit(212.0));
+        assertEquals("63°C", formatTemperatureFromFahrenheit(145.0));
+        assertEquals("74°C", formatTemperatureFromFahrenheit(165.0));
+    }
+
+    @Test
+    public void testFormatTemperatureFromFahrenheit_rounding() {
         assertEquals("100°F", formatTemperatureFromFahrenheit(100.499));
         assertEquals("101°F", formatTemperatureFromFahrenheit(100.5));
 
-        Locale.setDefault(Locale.GERMANY);
-        Locale.setDefault(Locale.Category.FORMAT, Locale.GERMANY);
+        setSystemLocales(Locale.GERMANY);
 
         assertEquals("0°C", formatTemperatureFromFahrenheit(32.0));
         assertEquals("100°C", formatTemperatureFromFahrenheit(212.0));
         assertEquals("63°C", formatTemperatureFromFahrenheit(145.0));
         assertEquals("74°C", formatTemperatureFromFahrenheit(165.0));
 
-        // Test rounding
         assertEquals("100°C", formatTemperatureFromFahrenheit(212.8)); // 100.444°C
         assertEquals("101°C", formatTemperatureFromFahrenheit(212.9)); // 100.5°C
+    }
+
+    @Test
+    public void testFormatTemperatureFromFahrenheit_negativeAndFreezing() {
+        assertEquals("-40°F", formatTemperatureFromFahrenheit(-40.0, Locale.US));
+        assertEquals("-40°C", formatTemperatureFromFahrenheit(-40.0, Locale.GERMANY));
+        assertEquals("0°F", formatTemperatureFromFahrenheit(0.0, Locale.US));
+        assertEquals("-18°C", formatTemperatureFromFahrenheit(0.0, Locale.GERMANY));
     }
 
     @Test
